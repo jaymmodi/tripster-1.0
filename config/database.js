7857dc37e924b6069600665a339edd34e60a3e1e
Ghanshyam Malu <malughanshyam@gmail.com>
2015-04-02 21:21:21 -0400

    Get and Post of Register.html is working, basically the Register page can now be used to create Users

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
 
 	// the database url to connect
-	url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
+//	url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
+	url : 'mongodb://tripster:tripster123@ds029640.mongolab.com:29640/tripster'
 }
